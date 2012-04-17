e7b54299a2acce0f7c761bb90e8c25076208d61b
showp1984 <showp1984@gmail.com>
2012-04-17 02:43:06 +0200

    sweep2wake_atmel: Add sweep2wake dependencies

--- drivers/leds/leds-pm8058.c
+++ drivers/leds/leds-pm8058.c
@@ -28,6 +28,10 @@
 #include <mach/htc_headset_misc.h>
 #endif
 
+#ifdef CONFIG_TOUCHSCREEN_ATMEL_SWEEP2WAKE
+#include <linux/atmel_qt602240.h>
+#endif
+
 #ifdef CONFIG_HTC_HEADSET_MISC
 #define charming_led_enable(enable) headset_indicator_enable(enable)
 #else
@@ -164,7 +168,7 @@ static void pm8058_pwm_led_brightness_set(struct led_classdev *led_cdev,
 	       ldata->bank, brightness);
 }
 
-static void pm8058_drvx_led_brightness_set(struct led_classdev *led_cdev,
+extern void pm8058_drvx_led_brightness_set(struct led_classdev *led_cdev,
 					   enum led_brightness brightness)
 {
 	struct pm8058_led_data *ldata;
@@ -588,6 +592,13 @@ static int pm8058_led_probe(struct platform_device *pdev)
 			goto err_register_attr_currents;
 		}
 	}
+
+#ifdef CONFIG_TOUCHSCREEN_ATMEL_SWEEP2WAKE
+	if (!strcmp(pdata->led_config[2].name, "button-backlight")) {
+		sweep2wake_setleddev(&ldata[2].ldev);
+		printk(KERN_INFO "[sweep2wake]: set led device %s, bank %d\n", pdata->led_config[2].name, ldata[2].bank);
+	}
+#endif
 
 	return 0;
 
